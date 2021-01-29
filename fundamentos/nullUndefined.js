1452199ff1667e53c892681d4fbb8761b4f5aead
Renan Oliveira <renan.sergipe68@gmail.com>
2021-01-29 19:19:11 -0300

    null undefined

---
+++ fundamentos/nullUndefined.js
@@ -0,0 +1,5 @@
+let valor;
+console.log(valor); //undefined= ela nao foi inicializada, não foi definido seu tipo ou valor
+
+valor = null;
+console.log(valor) //ela está nula, nao tem valor nenhum definido
---
+++ fundamentos/objeto.js
@@ -0,0 +1,14 @@
+const pessoa={} //criei ele vazio
+pessoa.nome= 'Renan' //nao criei a variavél isso porque posso criar dinamicamente ou seja, quando quiser.
+pessoa.idade=20
+pessoa.sexo='masculino'
+
+console.log(pessoa)
+
+const pessoa2={ //nesse caso estou criando o objeto já com seus atributos definidos
+    nome: 'emerson',
+    idade: 18,
+    sexo: 'masculino'
+}
+
+console.log(pessoa2)
