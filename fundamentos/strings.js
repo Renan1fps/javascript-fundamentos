29b2abcc4ebb09d15413c35a756cbe93c3d346b6
Renan Oliveira <renan.sergipe68@gmail.com>
2021-01-29 16:48:04 -0300

    Strings

---
+++ fundamentos/strings.js
@@ -0,0 +1,12 @@
+const nomes= "d3vdoj0"
+console.log(nomes.charAt(5))
+console.log(nomes.charAt(9)) //ele nao me retona nada
+console.log(nomes.indexOf('0')) //me retona a posiçao se tiver o numero que informei
+
+console.log(nomes.substring(1))// vai imprimir tudo a partir do index um
+
+console.log('willian '.concat(nomes).concat('br'))//concatenar strings
+
+console.log(nomes.replace(3,'e'))
+
+console.log("renan, lucas, emerson, gabriel".split(', ')) //transformando em array com split passando o parametro ', '
