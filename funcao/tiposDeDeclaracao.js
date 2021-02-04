5599858159e8e8853f4c32f8b29ab626d3e6b82d
Renan Oliveira <renan.sergipe68@gmail.com>
2021-02-04 15:52:08 -0300

    Declaração de funções

---
+++ funcao/tiposDeDeclaracao.js
@@ -0,0 +1,16 @@
+console.log(somar(3,9))
+
+//function declaration <--essa eu posso usar antes mesmo de declarar exemplo acima
+function somar(x, y) {
+  return x + y;
+}
+
+//function expression
+const somar2 = function (x, y) {
+  return x + y;
+};
+
+//named function expression
+const somar3 = function somar3(x, y) {
+  return x + y;
+};
