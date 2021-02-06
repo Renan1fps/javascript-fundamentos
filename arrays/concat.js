0a2c9eb9554681e6083d53bb0a77f259e7d89785
Renan Oliveira <renan.sergipe68@gmail.com>
2021-02-06 18:05:02 -0300

    Concat

---
+++ arrays/concat.js
@@ -0,0 +1,8 @@
+/**
+ * A função concat irá pegar dois arrays e juntar em um só gerando um novo array, podendo também adicionar novos, passando no parametro
+ */
+
+const filhos = ["francisco", "lucas"];
+const filhas = ["hinata", "isadora"];
+const todos = filhas.concat(filhos);
+console.log(todos);
