a9b85931b71db45c5f8fdd4e5d6d51ccd6d5b820
Renan Oliveira <renan.sergipe68@gmail.com>
2021-02-12 17:29:57 -0300

    Projeto API com express 2

--- node/miniProjeto/src/servidor.js
+++ node/miniProjeto/src/servidor.js
@@ -1,10 +1,26 @@
 const porta = 8080;
-const express = require("express");
 
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+const bancoDeDadados = require("./bancoDeDados");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/produtos", (req, res, next) => {
-  res.send({ produto: "notebook", valor: 2400.0 });
+  res.send(bancoDeDadados.getProdutos());
+});
+
+app.get("/produtos/:id", (req, res, next) => {
+  res.send(bancoDeDadados.getProduto(req.params.id));
+});
+
+app.post("/produtos", (req, res, next) => {
+  const produto = bancoDeDadados.salvarProdutos({
+    nome: req.body.nome,
+    preco: req.body.preco,
+  });
+  res.send(produto);
 });
 
 app.listen(porta, () => {
