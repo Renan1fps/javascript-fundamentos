c6a8163d4d13e145e2bf98c95acb601a082440c6
Renan Oliveira <renan.sergipe68@gmail.com>
2021-03-15 17:54:40 -0300

    Processando SASS com Gulp

---
+++ gulp/css/gulpfile.js
@@ -0,0 +1,20 @@
+const { series } = require("gulp");
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const uglifycss = require("gulp-uglifycss");
+const concat = require("gulp-concat");
+
+function tranformacaoCSS() {
+  return gulp
+    .src("src/sass/index.scss")
+    .pipe(sass().on("erro", sass.logError))
+    .pipe(
+      uglifycss({
+        uglyComments: true,
+      })
+    )
+    .pipe(concat("estilo.min.css"))
+    .pipe(gulp.dest('build/css'))
+}
+
+exports.default= series(tranformacaoCSS)
