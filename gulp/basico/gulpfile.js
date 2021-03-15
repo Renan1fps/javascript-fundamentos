28919355e2e636059f58c93b0624bd4d3798eeed
Renan Oliveira <renan.sergipe68@gmail.com>
2021-03-15 14:48:43 -0300

    Gulp 02

--- gulp/basico/gulpfile.js
+++ gulp/basico/gulpfile.js
@@ -1,10 +1,21 @@
+const { parallel } = require("gulp");
 const gulp = require("gulp");
-const series= gulp.series
+const series = gulp.series;
 
+const antes1 = (cb) => {
+  console.log("tarefa1");
+  return cb();
+};
+const antes2 = (cb) => {
+  console.log("tarefa2");
+  return cb();
+};
 
-function copiar(cb){
-    console.log('tarefa de copiar')
-    return cb()
+function copiar(cb) {
+  gulp
+    .src(["pastaA/arquivo1.txt", "pastaA/arquivo2.txt"])
+    .pipe(gulp.dest("pastaB"));
+  return cb();
 }
 
-module.exports.default=series(copiar)
+module.exports.default = series(parallel(antes1, antes2), copiar);
