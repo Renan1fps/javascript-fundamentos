2b56526dd41bc7dbb45456882f7580420facd978
Renan Oliveira <renan.sergipe68@gmail.com>
2021-03-16 17:33:02 -0300

    Tasks aplicação

--- gulp/spa/gulpTasks/deps.js
+++ gulp/spa/gulpTasks/deps.js
@@ -7,13 +7,13 @@ function depsCSS() {
     .src("node_modules/font-awesome/css/font-awesome.css")
     .pipe(uglifyCSS({ uglyComments: false }))
     .pipe(concat("deps.min.css"))
-    .pipe(gulp.dest("build/asserts/css"));
+    .pipe(gulp.dest("build/assets/css"));
 }
 function depsFonts() {
 
   return  gulp
   .src("node_modules/font-awesome/fonts/*.*")
-  .pipe(gulp.dest('build/asserts/fonts'))
+  .pipe(gulp.dest('build/assets/fonts'))
 }
 
 module.exports = {
