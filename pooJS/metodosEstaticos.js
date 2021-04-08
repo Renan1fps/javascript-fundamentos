7508ae7ed1d1d9aef0a2f72c2b67db4569f59733
Renan Oliveira <renan.sergipe68@gmail.com>
2021-04-08 12:58:55 -0300

    Metodos estaticos

---
+++ pooJS/metodosEstaticos.js
@@ -0,0 +1,13 @@
+class Calculadora {
+  static Somar(a, b) {
+    console.log(a + b);
+  }
+  static Subtrair(a, b) {
+    console.log(a - b);
+  }
+}
+
+
+//não necessita a criando do objeto
+Calculadora.Somar(10, 20);
+Calculadora.Subtrair(8, 5);
