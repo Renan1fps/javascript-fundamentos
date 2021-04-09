d3a52cca3b11b2c3b197bdf0aa84340f97029a60
Renan Oliveira <renan.sergipe68@gmail.com>
2021-04-09 17:51:56 -0300

    Listagem de dados

--- apiRest/index.js
+++ apiRest/index.js
@@ -5,8 +5,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/hello", (req, res) => {
-  res.send("Hello wold!");
+app.get("/games", (req, res) => {
+  res.statusCode = 200;
+  res.json(DB.games);
 });
 
 //banco de dados falso
@@ -16,19 +17,19 @@ var DB = {
       id: 23,
       name: "Call of duty",
       year: 2019,
-      price: 200
+      price: 200,
     },
     {
       id: 45,
       name: "Pubg",
       year: 2017,
-      price: 78
+      price: 78,
     },
     {
       id: 278,
       name: "Euro truck",
       year: 2011,
-      price: 10
+      price: 10,
     },
   ],
 };
