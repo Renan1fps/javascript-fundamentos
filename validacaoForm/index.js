227fddbb650ccbb07efc89432cd98131e71d6f48
Renan Oliveira <renan.sergipe68@gmail.com>
2021-05-18 14:57:22 -0300

    Lógica de validação

--- validacaoForm/index.js
+++ validacaoForm/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
-var session = require("express-session")
-var flash= require("express-flash")
+var session = require("express-session");
+var flash = require("express-flash");
+var cookieParser= require("cookie-parser")
 
+app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.use(express.json());
+
 app.use(
   session({
     secret: "keyboard cat",
@@ -13,10 +16,21 @@ app.use(
     cookie: { secure: true },
   })
 );
-app.use(flash())
+app.use(flash());
 
-app.get("/teste", (request, response) => {
-  response.json({ message: "hello!" });
+app.get("/", (request, response) => {
+  response.render("index");
+});
+
+app.post("/form", (request, response) => {
+  var { email, nome, pontos } = request.body;
+
+  //faço a validação que quiser
+  var nomeValidacao = "renan";
+  if (nome != nomeValidacao) {
+    response.redirect("/");
+  }
+  response.send(email);
 });
 
 app.listen(8181, () => {
