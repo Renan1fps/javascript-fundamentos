e88b7820f554f105644e079f4ef5834e8d591d59
Renan Oliveira <renan.sergipe68@gmail.com>
2021-05-18 15:16:55 -0300

    Finalização

--- validacaoForm/index.js
+++ validacaoForm/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 var session = require("express-session");
 var flash = require("express-flash");
-var cookieParser= require("cookie-parser")
+var cookieParser = require("cookie-parser");
 
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.use(express.json());
-app.use(cookieParser("20030927"))
+app.use(cookieParser("20030927"));
 app.use(
   session({
     secret: "keyboard cat",
@@ -19,7 +19,8 @@ app.use(
 app.use(flash());
 
 app.get("/", (request, response) => {
-  response.render("index");
+  var nomeError = request.flash("nomeError");
+  response.render("index",{nomeError});
 });
 
 app.post("/form", (request, response) => {
@@ -28,6 +29,8 @@ app.post("/form", (request, response) => {
   //faço a validação que quiser
   var nomeValidacao = "renan";
   if (nome != nomeValidacao) {
+    var nomeError = "Nome Errado!";
+    request.flash("nomeError", nomeError);
     response.redirect("/");
   }
   response.send(email);
